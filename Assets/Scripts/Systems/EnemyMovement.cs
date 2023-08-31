fd7350a737f61cb5e3c7c91144db26708f6e37c0
Tulgar Dinç <47505611+tulgardinc@users.noreply.github.com>
2023-08-31 05:06:34 +0200

    done

--- Assets/Scripts/Systems/EnemyMovement.cs
+++ Assets/Scripts/Systems/EnemyMovement.cs
@@ -1,18 +1,45 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
+[RequireMatchingQueriesForUpdate]
 public partial struct EnemyMovement : ISystem
 {
-    [BurstCompile]
-    public void OnUpdate (ref SystemState state)
+    EntityQuery query;
+
+    public void OnCreate(ref SystemState state)
     {
+        query = state.GetEntityQuery(ComponentType.ReadWrite<LocalTransform>(), ComponentType.ReadOnly<Speed>(), ComponentType.ReadOnly<EnemyTag>());
+    }
 
-        foreach (var (localTransform, speed, _) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Speed>, RefRO<EnemyTag>>())
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        float3 playerPos = float3.zero;
+        foreach (var (playerTransform, _) in SystemAPI.Query<RefRO<LocalTransform>, RefRO<PlayerTag>>())
         {
-            //float3 movementDirection = math.normalize(playerTransform.ValueRO.Position - localTransform.ValueRW.Position);
-            //localTransform.ValueRW.Position += movementDirection * speed.ValueRO.speed * Time.deltaTime;
+            playerPos = playerTransform.ValueRO.Position;
         }
+        new EnemyChaseJob
+        {
+            playerPosition = playerPos,
+            deltaTime = Time.deltaTime
+        }.ScheduleParallel(query);
+    }
 
+
+    [BurstCompile]
+    partial struct EnemyChaseJob : IJobEntity
+    {
+        public float3 playerPosition;
+        public float deltaTime;
+
+        void Execute(ref LocalTransform localTransform, ref Speed speed)
+        {
+            float3 movementDirection = math.normalize(playerPosition - localTransform.Position);
+            localTransform.Position += movementDirection * speed.speed * deltaTime;
+        }
     }
 }
