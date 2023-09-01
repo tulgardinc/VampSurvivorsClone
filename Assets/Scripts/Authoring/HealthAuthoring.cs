3bab27faef280a12003560c155aa3e66ab70bb0d
Capataina <47505622+Capataina@users.noreply.github.com>
2023-09-02 01:50:29 +0300

    fixes

--- Assets/Scripts/Authoring/HealthAuthoring.cs
+++ Assets/Scripts/Authoring/HealthAuthoring.cs
@@ -1,6 +1,20 @@
+using Unity.Entities;
 using UnityEngine;
 
 public class HealthAuthoring : MonoBehaviour
 {
+
+    public float health;
+
+    public class HealthBaker : Baker <HealthAuthoring>
+    {
+
+        public override void Bake (HealthAuthoring authoring)
+        {
+            var entity = GetEntity (TransformUsageFlags.Dynamic);
+            AddComponent (entity, new Health { health = authoring.health });
+        }
+
+    }
 
 }
