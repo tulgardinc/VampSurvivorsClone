2a90db0adebff423f1cb0886ece3af9559f1c5cb
Capataina <47505622+Capataina@users.noreply.github.com>
2023-09-04 01:20:34 +0300

    awdawd

--- Assets/Scripts/Systems/XPOrbFollowPlayerSystem.cs
+++ Assets/Scripts/Systems/XPOrbFollowPlayerSystem.cs
@@ -1,36 +1,33 @@
-
-
 using Unity.Burst;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Physics;
-using Unity.Physics.Systems;
 using Unity.Transforms;
 using UnityEngine;
-
 
 [RequireMatchingQueriesForUpdate]
 public partial struct XPOrbFollowPlayerSystem : ISystem
 {
+
     private EntityQuery queryFollow;
     private EntityQuery queryAddComponent;
 
     public void OnCreate(ref SystemState state)
     {
-        queryFollow = state.GetEntityQuery(ComponentType.ReadWrite<LocalTransform>(), ComponentType.ReadOnly<Speed>(),
-           ComponentType.ReadWrite<OrbFollowingPlayer>());
-        queryAddComponent = state.GetEntityQuery(ComponentType.ReadOnly<LocalTransform>(), ComponentType.ReadOnly<Experience>(), ComponentType.Exclude<OrbFollowingPlayer>());
-        state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+        queryFollow = state.GetEntityQuery(ComponentType.ReadWrite <LocalTransform>(), ComponentType.ReadOnly <Speed>(),
+                                           ComponentType.ReadWrite <OrbFollowingPlayer>());
+        queryAddComponent = state.GetEntityQuery(ComponentType.ReadOnly <LocalTransform>(),
+                                                 ComponentType.ReadOnly <Experience>(),
+                                                 ComponentType.Exclude <OrbFollowingPlayer>());
+        state.RequireForUpdate <EndSimulationEntityCommandBufferSystem.Singleton>();
     }
 
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (playerTransform, canPickupXP) in SystemAPI.Query<RefRO<LocalTransform>, RefRO<CanPickupXP>>())
+        foreach (var (playerTransform, canPickupXP) in SystemAPI.Query <RefRO <LocalTransform>, RefRO <CanPickupXP>>())
         {
-            var ecbSystem = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+            var ecbSystem = SystemAPI.GetSingleton <EndSimulationEntityCommandBufferSystem.Singleton>();
             var ecb = ecbSystem.CreateCommandBuffer(state.WorldUnmanaged);
-            EntityCommandBuffer.ParallelWriter parallelEcb = ecb.AsParallelWriter();
+            var parallelEcb = ecb.AsParallelWriter();
 
             new XPOrbShouldFollowJob
             {
@@ -43,7 +40,7 @@ public partial struct XPOrbFollowPlayerSystem : ISystem
             new XPOrbChaseJob
             {
                 playerPosition = playerTransform.ValueRO.Position,
-                deltaTime = Time.fixedDeltaTime
+                deltaTime = Time.deltaTime
             }.ScheduleParallel(queryFollow);
         }
     }
@@ -51,6 +48,7 @@ public partial struct XPOrbFollowPlayerSystem : ISystem
     [BurstCompile]
     private partial struct XPOrbShouldFollowJob : IJobEntity
     {
+
         public float3 playerPosition;
         public float range;
         public EntityCommandBuffer.ParallelWriter ecb;
@@ -61,24 +59,27 @@ public partial struct XPOrbFollowPlayerSystem : ISystem
             var distance = math.distance(playerPosition, orbTransform.Position);
             if (distance < range)
             {
-                ecb.AddComponent(sortKey, entity, new OrbFollowingPlayer { });
+                ecb.AddComponent(sortKey, entity, new OrbFollowingPlayer());
             }
         }
+
     }
 
     [BurstCompile]
     private partial struct XPOrbChaseJob : IJobEntity
     {
+
         public float3 playerPosition;
         public float deltaTime;
 
         private void Execute(ref LocalTransform orbTransform,
-            ref Speed speed, ref OrbFollowingPlayer orbFollowingPlayer)
+                             ref Speed speed, ref OrbFollowingPlayer orbFollowingPlayer)
         {
             var movementDirection = math.normalize(playerPosition - orbTransform.Position);
             orbFollowingPlayer.currentSpeed += speed.speed * deltaTime;
             orbTransform.Position += movementDirection * orbFollowingPlayer.currentSpeed * deltaTime;
         }
+
     }
 
 
--- Assets/Scripts/Systems/XPOrbSpawnMotionSystem.cs
+++ Assets/Scripts/Systems/XPOrbSpawnMotionSystem.cs
@@ -1,18 +1,21 @@
-using System.Timers;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
-using UnityEngine.Video;
 
 public partial struct XPOrbSpawnMotionSystem : ISystem
 {
 
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate <OrbSpawner>();
+        state.RequireForUpdate <EndSimulationEntityCommandBufferSystem.Singleton>();
+    }
+
 
     public void OnUpdate(ref SystemState state)
     {
-        var ecbSystem = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
-        float deceleration = SystemAPI.GetSingleton<OrbSpawner>().orbSpawnDeceleration;
+        var ecbSystem = SystemAPI.GetSingleton <EndSimulationEntityCommandBufferSystem.Singleton>();
+        var deceleration = SystemAPI.GetSingleton <OrbSpawner>().orbSpawnDeceleration;
         var ecb = ecbSystem.CreateCommandBuffer(state.World.Unmanaged);
         var parallelEcb = ecb.AsParallelWriter();
 
@@ -27,21 +30,25 @@ public partial struct XPOrbSpawnMotionSystem : ISystem
 
     public partial struct XPOrbSpawnMotionJob : IJobEntity
     {
+
         public EntityCommandBuffer.ParallelWriter ecb;
         public float timeDelta;
         public float deceleration;
 
-        public void Execute([ChunkIndexInQuery] int sortKey, ref LocalTransform transform, ref XPOrbHasStartMotion xpOrbHasStartMotion, Entity entity)
+        private void Execute([ChunkIndexInQuery] int sortKey, ref LocalTransform transform,
+                             ref XPOrbHasStartMotion xpOrbHasStartMotion, Entity entity)
         {
             if (xpOrbHasStartMotion.currentSpeed <= 0)
             {
-                ecb.RemoveComponent<XPOrbHasStartMotion>(sortKey, entity);
+                ecb.RemoveComponent <XPOrbHasStartMotion>(sortKey, entity);
             }
             else
             {
                 transform.Position += xpOrbHasStartMotion.direction * xpOrbHasStartMotion.currentSpeed * timeDelta;
-                xpOrbHasStartMotion.currentSpeed -= deceleration;
+                xpOrbHasStartMotion.currentSpeed -= deceleration * timeDelta;
             }
         }
+
     }
+
 }
